45e8b66bf3f250b81a2fafaf7e1f53d9d4b94098
Pabloocp <pablocastropaz00@gmail.com>
2022-04-04 12:06:43 +0200

    add method

--- src/main/java/com/example/demo/controllers/Ejericio.java
+++ src/main/java/com/example/demo/controllers/Ejericio.java
@@ -2,10 +2,13 @@ package com.example.demo.controllers;
 
 
 
+import java.text.MessageFormat;
+
 import com.example.demo.utils.Utils;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,4 +29,15 @@ public class Ejericio {
        boolean espalindromo = Utils.EsPalindromoBoolean(name);
        return espalindromo ? "Es palindromo" : "No es palindromo";
     }
+
+    //via query
+
+    @GetMapping("/sumar")
+    public String add(@RequestParam String num1,@RequestParam String num2){
+        int resultado = Integer.parseInt(num1) + Integer.parseInt(num2);
+        Object params[] = {num1,num2,resultado};
+        return MessageFormat.format("La suma de {0} y {1} es {2}", params );
+    }
+
+
 }
